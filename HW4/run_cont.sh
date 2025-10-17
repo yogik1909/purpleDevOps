31581c7882bc4a6414e9dd032a389b5809674a73
yog <white_net@mail.ru>
2025-10-17 23:15:44 +1000

    fix

--- HW4/run_cont.sh
+++ HW4/run_cont.sh
@@ -1,1 +1,5 @@
-docker run -v /home/vasa/data:/var/lib/postgresql -p 65432:5432 -e POSTGRES_PASSWORD=password -d postgres
+docker run -v /home/vasa:/var/lib/postgresql -p 65432:5432 -e POSTGRES_PASSWORD=password -d postgres
+docker stop angry_hellman
+docker rm angry_hellman
+docker run -v /home/vika:/var/lib/postgresql -p 65432:5432 -e POSTGRES_PASSWORD=password -d postgres
+docker ps
