4f4900a295a50876a8fd602664b2ffe9cf326f37
Vova <white_net@mail.ru>
2025-10-22 17:43:53 +1000

    fix 1

--- 9-ansible-stats/script.sh
+++ 9-ansible-stats/script.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-ansible -i host.ini -m setup -a "filter=env,system,distribution*,ansible_hostname" demo
+ansible -i host.ini -m setup -a "filter=env,system,distribution*,ansible_hostname" demo | tee info.txt
